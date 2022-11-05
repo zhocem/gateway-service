a32d2927d5121fbf7d9b00066eed6eabfb68ad09
zhocem <pasgeekdutout@gmail.com>
2022-11-05 14:07:52 +0100

    initial commit with static routing

---
+++ src/test/java/fr/dovi/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dovi.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
